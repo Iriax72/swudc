259daecae5d9ddc2dafe2a562622601186ed384f
flexult <touma.leandre.72@gmail.com>
2026-02-22 06:48:46 +0000

    4 boutons basiques

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+    <?php
+    require './elements/head.php';
+    ?>
+    <body>
+        <?php
+        require './pages/menu.php';
+        ?>
+    </body>
+</html>
---
+++ pages/menu.php
@@ -0,0 +1,4 @@
+<button>Vos Decks</button>
+<button>Nouveau Deck</button>
+<button>Communauté</button>
+<button>Réglages</button>
