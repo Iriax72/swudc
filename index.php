ae17f8f91b51ec53d1c80f3d51bd5dd167e4e2eb
flexult <touma.leandre.72@gmail.com>
2026-03-03 08:23:55 +0000

    patch

--- index.php
+++ index.php
@@ -1,11 +1,25 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
     <?php
     require './elements/head.php';
     ?>
     <body>
         <?php
-        require './pages/menu.php';
+        // Routage simple depuis le paramètre GET `page`.
+        $allowed = [
+            'menu' => './pages/menu.php',
+            'yours' => './pages/yours.php',
+            'new' => './pages/new.php',
+            'community' => './pages/community.php',
+            'settings' => './pages/settings.php',
+        ];
+
+        $page = isset($_GET['page']) ? basename($_GET['page']) : 'menu';
+        if (!array_key_exists($page, $allowed)) {
+            $page = 'menu';
+        }
+
+        require $allowed[$page];
         ?>
     </body>
 </html>
