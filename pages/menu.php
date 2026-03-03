e2a284a2673da8eeced0fd72928329037354b428
flexult <touma.leandre.72@gmail.com>
2026-03-03 08:16:32 +0000

    meilleures ancree

--- pages/menu.php
+++ pages/menu.php
@@ -1,7 +1,7 @@
 <h1>SWUDC</h1>
 <div id="a-container">
-    <a link="./yours.php" class="menu-a">Vos Decks</a>
-    <a link="./new.php" class="menu-a">Nouveau Deck</a>
-    <a link="./community.php" class="menu-a">Communauté</a>
-    <a link="./settings.php" class="menu-a">Réglages</a>
+    <a href="./yours.php" class="menu-a">Vos Decks</a>
+    <a href="./new.php" class="menu-a">Nouveau Deck</a>
+    <a href="./community.php" class="menu-a">Communauté</a>
+    <a href="./settings.php" class="menu-a">Réglages</a>
 </div>
