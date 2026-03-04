c97fd7af464f77ffd4ddcd33c1cdbb59fd483c97
flexult <touma.leandre.72@gmail.com>
2026-03-04 12:23:28 +0000

    new.php

--- pages/new.php
+++ pages/new.php
@@ -2,3 +2,20 @@
 require('./elements/backBtn.php');
 ?>
 <h1>Nouveau Deck</h1>
+<div id="newdeck-container">
+    <button class="card-btn leader">
+        <img src="./assets/images/plus-icon.png" alt="plus icon">
+    </button>
+    <button class="card-btn base">
+        <img src="./assets/images/plus-icon.png" alt="plus icon">
+    </button>
+    <div id="newdeck-base-container">
+        <?php
+        for ($i = 0; $i < 50; $i++) {
+            echo '<button class="card-btn">
+                <img src="./assets/images/plus-icon.png" alt="plus icon">
+            </button>';
+        }
+        ?>
+    </div>
+</div>
